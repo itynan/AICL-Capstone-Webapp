5f28c3a5e14228688e02771920e19ea134ebac9d
Iliya Tynan <itynan89@gmail.com>
2024-03-13 19:10:17 -0500

    rendering datasets.html

--- webflow_template/server.py
+++ webflow_template/server.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/datasets.html')
+def datasets():
+    return render_template('datasets.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
