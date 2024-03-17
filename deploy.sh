3febdeeee7561cd5337cdfa439b19c84bd4ef2b7
Iliya Tynan <itynan89@gmail.com>
2024-03-17 00:30:58 -0500

    set up GHA deploy

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+
+source ~/myenv/bin/activate
+pip install -r requirements.txt
+
+# Start your Dash application
+python dash-ml-app.py
+python webflow-fe-server.py
--- webflow_template/webflow-fe-server.py
+++ webflow_template/webflow-fe-server.py
@@ -14,4 +14,4 @@ def f1models():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
