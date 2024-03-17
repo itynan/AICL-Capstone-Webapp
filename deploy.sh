8f2b15f21c38aa45e2198621766f0ff14e1156ce
Iliya Tynan <itynan89@gmail.com>
2024-03-17 00:37:23 -0500

    corrected gha deply yaml and deploy-sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ source ~/myenv/bin/activate
 pip install -r requirements.txt
 
 # Start your Dash application
-python dash-ml-app.py
-python webflow-fe-server.py
+python dash-ml-app.py &
+python webflow-fe-server.py &
