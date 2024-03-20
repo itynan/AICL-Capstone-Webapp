552aa0902e72f9b2786c76632b07dca6bda2a8f1
Iliya Tynan <itynan89@gmail.com>
2024-03-20 00:09:58 -0500

    add gunicorn config files

---
+++ dash/gunicorn_config_port_7001.py
@@ -0,0 +1,6 @@
+
+# gunicorn_config.py
+workers = 4
+timeout = 30
+bind = "0.0.0.0:7001"
+loglevel = "info"
---
+++ webflow_template/gunicorn_config_port_5000.py
@@ -0,0 +1,8 @@
+
+# gunicorn_config.py
+workers = 4
+timeout = 30
+bind = "0.0.0.0:5000"
+loglevel = "info"
+
+
